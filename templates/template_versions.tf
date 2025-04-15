09775b8de617c7dff912ea4e5a3067ca4b0abc80
Greg <greg@coder.com>
2025-04-15 22:04:06 +0000

    testing gitops template management

---
+++ templates/template_versions.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    coderd = {
+      source = "coder/coderd"
+    }
+  }
+}
+// Provider populated from environment variables
+provider "coderd" {
+    url   = "${env("CODER_AGENT_URL")}"
+    token = "${env("CODER_AGENT_TOKEN")}"
+}
+
+resource "coderd_template" "kubernetes-base" {
+  name        = Kubernetes (Deployment)-GitOps"
+  description = "Provision Kubernetes Deployments as Coder workspaces."
+  versions = [
+    {
+      name        = "stable-test1"
+      description = "The stable version of the template."
+      directory   = "./kubernetes-base"
+    },
+  ]
+  acl = {
+    groups = []
+  }
+}
