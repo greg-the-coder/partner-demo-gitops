dae21f0d3d40d68c7926454eeadc888edb269519
Greg <greg@coder.com>
2025-05-13 19:02:43 +0000

    updated tf template resources

--- templates/template_versions.tf
+++ templates/template_versions.tf
@@ -111,7 +111,7 @@ resource "coderd_template" "aws-devcontainer" {
 }
 
 resource "coderd_template" "aws-devcontainer-br" {
-  name        = "aws-devcontainer"
+  name        = "aws-devcontainer-br"
   display_name = "Devcontainers (AWS EC2 Linux/Bottlerocket)"
   description = "Provision envbuilder containers on AWS EC2 VMs using Amazon Bottlerocket OS as Coder workspaces"
   icon = "/icon/aws.png"
