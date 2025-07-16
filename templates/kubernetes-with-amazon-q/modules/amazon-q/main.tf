ef919c028814bfc4b5a1a3d965cad7ec553534cc
Greg <greg@coder.com>
2025-07-16 21:12:48 +0000

    remove package upgrade

--- templates/kubernetes-with-amazon-q/modules/amazon-q/main.tf
+++ templates/kubernetes-with-amazon-q/modules/amazon-q/main.tf
@@ -120,7 +120,6 @@ resource "coder_script" "amazon_q" {
     #!/bin/bash
     set -o errexit
     set -o pipefail
-    sudo apt update -y
     sudo apt install zstd
 
     # Function to check if a command exists
