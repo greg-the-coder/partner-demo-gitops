348a08fcab10ef97b33749e69c8519fe7261dc6c
Greg <greg@coder.com>
2025-10-09 20:23:41 +0000

    updating rds naming to be dynamic

--- templates/kubernetes-awsrag-with-claude-code/main.tf
+++ templates/kubernetes-awsrag-with-claude-code/main.tf
@@ -200,7 +200,7 @@ module "code-server" {
 module "claude-code" {
     count               = data.coder_workspace.me.start_count
     source              = "registry.coder.com/coder/claude-code/coder"
-    version             = "2.0.0"
+    version             = "3.1.0"
     agent_id            = coder_agent.dev.id
     folder              = local.home_folder
     subdomain           = false
