cb4b3b050d26b20d6b42f41bccbc9c8e95d2ddd5
Greg <greg@coder.com>
2025-12-22 20:58:13 +0000

    prototyping jupyter with Claude Code template

--- templates/awshp-k8s-tasks-with-cc-jupyter/main.tf
+++ templates/awshp-k8s-tasks-with-cc-jupyter/main.tf
@@ -107,16 +107,7 @@ locals {
     port = 3000
     domain = element(split("/", data.coder_workspace.me.access_url), -1)
     access_url = tostring(data.coder_workspace.me.access_url)
-    jupyter_config = "{
-        ServerApp = {
-          # Required for Coder Tasks iFrame embedding - do not remove
-          tornado_settings = {
-            headers = {
-              "Content-Security-Policy" = "frame-ancestors 'self' ${data.coder_workspace.me.access_url}"
-            }
-          }
-        }
-      }"
+    jupyter_config = "{ ServerApp = { tornado_settings = { headers = { "Content-Security-Policy" = "frame-ancestors 'self' ${data.coder_workspace.me.access_url}" } } }"
 }
 
 locals {
