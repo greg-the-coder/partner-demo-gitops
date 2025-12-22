9b77dcaf2ed6b87066c02987b60fb51268d1beb6
Greg <greg@coder.com>
2025-12-22 20:19:55 +0000

    prototyping jupyter with Claude Code template

--- templates/template_versions.tf
+++ templates/template_versions.tf
@@ -53,6 +53,23 @@ resource "coderd_template" "awshp-k8s-with-claude-code" {
   }]
 }
 
+resource "coderd_template" "awshp-k8s-tasks-with-cc-jupyter" {
+  name        = "awshp-k8s-tasks-with-cc+jupyter"
+  display_name = "AWS Workshop - Kubernetes with Claude Code + Jupyter lab"
+  description = "Provision Kubernetes Deployments as Coder workspaces with Anthropic Claude Code and Jupyter lab."
+  icon = "/icon/k8s.png"
+  versions = [{
+    directory = "./awshp-k8s-tasks-with-cc-jupyter"
+    active    = true
+    # Version name is optional
+    name = var.coder_gitsha
+    tf_vars = [{
+      name  = "namespace"
+      value = "coder"
+    }]
+  }]
+}
+
 resource "coderd_template" "awshp-linux-q-base" {
   name        = "awshp-linux-q-base"
   display_name = "AWS Workshop - EC2 (Linux) Q Developer"
